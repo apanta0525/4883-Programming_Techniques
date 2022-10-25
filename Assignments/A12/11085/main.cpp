7ac179476f8f7378d2cc65a974bfb33ae02b7f1f
Aashish Panta <112501963+apanta0525@users.noreply.github.com>
2022-10-25 12:26:59 +0300

    Update main.cpp

--- Assignments/A12/11085/main.cpp
+++ Assignments/A12/11085/main.cpp
@@ -1,19 +1,33 @@
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <vector>
+
 using namespace std;
 
+vector<int> string_split(string str) {
+    vector<int> result;
+    istringstream iss(str);
+    for (int s; iss >> s;)
+        result.push_back(s);
+    return result;
+}
 
-int main() {
+int process(vector<int> nums, int cases) {
 
-    int chessBoard[8][8] = {}; 
+    int chessBoard[8][8] = {};
 
     int row = 0, col = 0;
     int moves = 0;
     int minMoves = 1000;
-    cout << " Enter Tests: ";
     int count = 0;
-    while (cin >> row) {
-        col = row;
+
+
+
+    for (int x = 0; x < nums.size(); x++) {
+        col = row = nums[x];
         chessBoard[row - 1][col - 1] = 1; //place queens
         count++;
 
@@ -24,7 +38,8 @@ int main() {
 
     //we will start at row = 0, col = 0
     row = 0, col = 0;
-    
+    cout << endl;
+
 
 
 
@@ -59,7 +74,7 @@ tryBackTrack:
     col--;
     moves--;
     if (col == -1) {
-        cout << "Min Moves " << minMoves << " ";
+        cout << "Case "<<cases <<":" << minMoves << " ";
         return 0;
     }
     //update visited
@@ -78,5 +93,24 @@ updateMinMoves:
     }
     moves = 0;
     goto tryBackTrack;
+}
+
+int main() {
+
+    fstream newfile;
+    newfile.open("input.txt", ios::in);
+    int cases = 1;
+    if (newfile.is_open()) { //checking whether the file is open
+        string tp;
+        while (getline(newfile, tp)) { //read data from file object and put it into string.
+            vector<int> nums = string_split(tp);
+            process(nums,cases);
+            
+            cases++;
+        }
+        newfile.close(); //close the file object.
+    }
+
+
 }
 
